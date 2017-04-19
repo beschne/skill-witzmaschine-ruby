efc72c9ddcbb854c61eab65b324b83b2f3e99912
Benno Schneider <Benno.Schneider@gmail.com>
2017-04-19 17:12:27 +0200

    Added processing when Skill launched. Closes #26.

--- sinatra/handler.rb
+++ sinatra/handler.rb
@@ -5,17 +5,25 @@ require 'alexa_skills_ruby'
 require_relative 'joke'
 
 class Handler < AlexaSkillsRuby::Handler 
-                         
+
+  on_launch() do 
+    createJoke
+    logger.info 'on_launch processed'  
+  end
+  
   on_intent('tellMeAJoke') do  
+    createJoke
+    logger.info 'tellMeAJoke processed'
+  end
+   
+  def createJoke 
     joke = Joke.new
-    # slots = request.intent.slots
     response.set_output_speech_text(joke.text)
     response.set_output_speech_ssml('<speak>' + joke.speech + '</speak>')
     response.set_reprompt_speech_text(joke.text)
     response.set_reprompt_speech_ssml('<speak><s>Ich wiederhole:</s>' + joke.speech + '</speak>')
     response.set_simple_card('Witzmaschine', joke.text) 
     response.should_end_session = true
-    logger.info 'tellMeAJoke processed'
   end
   
 end
